398405ed792b265c4b5060dfe8a2b2a2f5a86515
andresvia <andres.via@gmail.com>
2016-05-25 02:35:50 -0500

    no dependency on metainit, preparing for rpm

--- package/fpm.sh
+++ package/fpm.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 set -eu
-fpm --verbose -C package/deb -s dir -t deb -n udpac -v "$TRAVIS_TAG" -d metainit --after-install package/deb-after-install
+fpm --verbose -C package/deb -s dir -t deb -n udpac -v "$TRAVIS_TAG" --after-install package/deb-after-install .
+fpm --verbose -C package/rpm -s dir -t rpm -n udpac -v "$TRAVIS_TAG" --after-install package/rpm-after-install .
